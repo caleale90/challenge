132ee74f217e1d8c13ff14601beb05019ff59e48
alessandro <caleale90@gmail.com>
2024-12-17 20:25:51 +0100

    aggiunta query di utilità

--- scripts/db.sh
+++ scripts/db.sh
@@ -8,6 +8,7 @@ docker exec -it postgres bash -c "psql -U postgres -d challenge"
 
 # Tutte le tabelle assieme
 # SELECT * FROM public.ratings JOIN public.movies ON movies.id = ratings.movie_id JOIN public.users ON users.id = ratings.user_id;
+# SELECT title, username, rating, view_percentage, implicit_rating FROM public.ratings JOIN public.movies ON movies.id = ratings.movie_id JOIN public.users ON users.id = ratings.user_id;
 
 # Movies
 # SELECT * FROM public.movies;
