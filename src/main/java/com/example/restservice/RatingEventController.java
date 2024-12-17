b4db2f0935009eed7e1d3356dac847f961f0fd29
alessandro <caleale90@gmail.com>
2024-12-17 17:34:08 +0100

    Aggiunta classe RatingEventController

--- src/main/java/com/example/restservice/RatingEventController.java
+++ src/main/java/com/example/restservice/RatingEventController.java
@@ -1,37 +1,23 @@
 package com.example.restservice;
 
-import model.Movie;
-import model.Rating;
-import model.User;
 import org.springframework.stereotype.Service;
-
-import java.sql.SQLException;
 
 @Service
 public class RatingEventController extends EventController {
 
-    public RatingEventController() throws SQLException {
-        super();
+    public RatingEventController(MovieRepository movieRepository, UserRepository userRepository, RatingRepository ratingRepository) {
+        super(movieRepository, userRepository, ratingRepository);
     }
 
-    public void saveRating(User user, Movie movie, Rating rating) throws SQLException {
-        int movieId = getMovieId(movie.getTitle());
-        int userId = getUserId(user.getUsername());
-        if (ratingExists(user, movie)) {
-            updateRating(userId, movieId, rating.getRatingValue(), false);
+    public void saveRating(String username, String movieTitle, Integer rating) {
+        Long movieId = movieRepository.findIdByTitle(movieTitle).orElseThrow(() -> new RuntimeException("Movie not found"));;
+        Long userId = userRepository.findUserIdByUsername(username).orElseThrow(() -> new RuntimeException("User not found"));;
+
+        if (ratingExists(userId, movieId)) {
+            ratingRepository.updateRatingAndImplicitRating(rating,false, userId, movieId);
         } else {
-            storeRating(userId, movieId, rating.getRatingValue());
+            ratingRepository.insertRating(userId, movieId, rating);
         }
-    }
-
-    protected void updateRating(int userId, int movieId, int rating, boolean implicitRating) throws SQLException {
-        String query = "UPDATE public.ratings SET rating = ?, implicit_rating = ? WHERE user_id = ? AND movie_id = ?";
-        databaseConnection.updateRatingQuery(query, rating, implicitRating, userId, movieId);
-    }
-
-    protected void storeRating(int userId, int movieId, int rating) throws SQLException {
-        String query = "INSERT INTO public.ratings (user_id, movie_id, rating, implicit_rating) VALUES (?, ?, ?, false)";
-        databaseConnection.insertQuery(query, userId, movieId, rating);
     }
 
 }
