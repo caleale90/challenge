4b4e1894b624443ec0ab95f8c75fe6aa26e5d16b
alessandro <caleale90@gmail.com>
2024-12-18 18:30:39 +0100

    corretto nome parametro

--- scripts/recommendation_system.sh
+++ scripts/recommendation_system.sh
@@ -12,11 +12,11 @@ elif [[ "$1" == "stop" ]]; then
     echo "Stopping the Recommendation system..."
     bash stop.sh
     echo "Recommendation system stopped"
-elif [[ "$1" == "stop" ]]; then
+elif [[ "$1" == "initialize" ]]; then
     echo "Initializing the Recommendation system..."
     bash provision.sh
     echo "Recommendation system initialized and started..."
 else
-    echo "Usage: $0 {start|stop}"
+    echo "Usage: $0 {start|stop|initialize}"
     exit 1
 fi
