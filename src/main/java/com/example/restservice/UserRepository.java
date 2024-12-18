02a04a8b622a17220e824705d9e35734fc8e600b
alessandro <caleale90@gmail.com>
2024-12-18 19:04:28 +0100

    rimosso metodo inutilizzato

--- src/main/java/com/example/restservice/UserRepository.java
+++ src/main/java/com/example/restservice/UserRepository.java
@@ -8,8 +8,6 @@ import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
-    Optional<Long> findByUsername(String username);
-
     @Query("SELECT u.id FROM User u WHERE u.username = :username")
     Optional<Long> findUserIdByUsername(@Param("username") String username);
 
