edb046c486d48dfcf6fb7046f82f1cd0af5e815b
alessandro <caleale90@gmail.com>
2024-12-18 17:48:24 +0100

    aggiunto script di utilità per servizio

---
+++ scripts/recommendation_service.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+set -e
+
+if [[ "$1" == "start" ]]; then
+    echo "Starting the Recommendation system..."
+    bash start.sh
+    echo "Recommendation system started..."
+elif [[ "$1" == "stop" ]]; then
+    echo "Stopping the Recommendation system..."
+    bash stop.sh
+    echo "Recommendation system stopped"
+elif [[ "$1" == "stop" ]]; then
+    echo "Initializing the Recommendation system..."
+    bash provision.sh
+    echo "Recommendation system initialized and started..."
+else
+    echo "Usage: $0 {start|stop}"
+    exit 1
+fi
